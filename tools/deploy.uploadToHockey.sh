886331752061c1eb772047f3a6579f3b8e25bdcf
daniel.boehrs <daniel.boehrs@open-xchange.com>
2019-04-15 10:10:25 +0200

    BUILD Add status script

--- tools/deploy.uploadToHockey.sh
+++ tools/deploy.uploadToHockey.sh
@@ -3,13 +3,13 @@
 set -e
 set -o pipefail
 
-RELEASE_NOTES=`cat tools/build.deploy.release-notes-hockey-dev.md`
+RELEASE_NOTES=`cat tools/deploy.releaseNotesHockeyDev.md`
 
 curl \
   -F "status=2" \
   -F "notify=1" \
   -F "notes=$RELEASE_NOTES" \
   -F "notes_type=1" \
-  -F "ipa=@build/app/outputs/apk/release/app-release.apk" \
+  -F "ipa=@build/app/outputs/apk/release/app-development-release.apk" \
   -H "X-HockeyAppToken: $BUILD_TOKEN" \
   https://rink.hockeyapp.net/api/2/apps/${HOCKEY_APP_ID}/app_versions/upload
