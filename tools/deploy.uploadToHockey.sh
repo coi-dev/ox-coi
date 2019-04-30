400c0f207e42df1fdc9179d91f2e0ce90814e7bc
daniel.boehrs <daniel.boehrs@open-xchange.com>
2019-04-30 11:45:10 +0200

    BUILD adjust upload source

--- tools/deploy.uploadToHockey.sh
+++ tools/deploy.uploadToHockey.sh
@@ -10,6 +10,6 @@ curl \
   -F "notify=1" \
   -F "notes=$RELEASE_NOTES" \
   -F "notes_type=1" \
-  -F "ipa=@build/app/outputs/apk/release/app-development-release.apk" \
+  -F "ipa=@build/app/outputs/apk/development/release/app-development-release.apk" \
   -H "X-HockeyAppToken: $BUILD_TOKEN" \
   https://rink.hockeyapp.net/api/2/apps/${HOCKEY_APP_ID}/app_versions/upload
