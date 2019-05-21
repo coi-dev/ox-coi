c5a975fe67b37f7e5b3b7cc8228eb8f13ff0d5f7
daniel.boehrs <daniel.boehrs@open-xchange.com>
2019-05-21 11:15:28 +0200

    BUILD Review fixes

--- tools/build.triggerBuild.sh
+++ tools/build.triggerBuild.sh
@@ -3,18 +3,17 @@
 set -e
 set -o pipefail
 
-PROJECT_FOLDER=`pwd`
 PLUGIN_FOLDER="flutter-deltachat-core";
+BUILD_NAME="oxdev-r999.0"
 
-BUILD_NAME="oxdev-r999.0-android-b$BUILD_ID"
-
-cd ..
-if [[ -d "$PLUGIN_FOLDER" ]]; then
-    cd ${PLUGIN_FOLDER}
-    git pull
-    git submodule update
-else
-    git clone --recurse-submodules https://github.com/open-xchange/flutter-deltachat-core.git
-fi
-cd ${PROJECT_FOLDER}
-flutter build apk --build-name=${BUILD_NAME} --build-number=${BUILD_ID} --flavor development
+(
+    cd ..
+    if [[ -d "$PLUGIN_FOLDER" ]]; then
+        cd ${PLUGIN_FOLDER}
+        git pull
+        git submodule update
+    else
+        git clone --recurse-submodules https://github.com/open-xchange/flutter-deltachat-core.git
+    fi
+)
+flutter build apk --build-name=${BUILD_NAME} --build-number=${CI_PIPELINE_ID} --flavor development
