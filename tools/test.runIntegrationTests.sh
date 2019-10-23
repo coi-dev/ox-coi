d88692256ebbafa55eac496be086767f6d718432
daniel.boehrs <daniel.boehrs@open-xchange.com>
2019-10-23 15:22:01 +0200

    Add missing checks

--- tools/test.runIntegrationTests.sh
+++ tools/test.runIntegrationTests.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-LOG_FILE=test_driver/logs/log.txt
+LOG_FOLDER=test_driver/logs
+LOG_FILE=${LOG_FOLDER}/log.txt
 success=0
 failed=0
 
+cd ..
+if [[ ! -d "$LOG_FOLDER" ]]; then
+    mkdir ${LOG_FOLDER}
+fi
 if [[ -f "$LOG_FILE" ]]; then
     rm ${LOG_FILE}
 fi
@@ -13,7 +18,7 @@ for test in test_driver/*
 do
     if [[ -f "$test" ]]; then
         echo "Running test: $test"
-        flutter drive --target=test_driver/setup/app.dart --driver=${test} --flavor development >> $LOG_FILE 2>&1
+        flutter drive --target=test_driver/setup/app.dart --driver=${test} --flavor development >> ${LOG_FILE} 2>&1
         if [[ $? -eq 0 ]]; then
             echo "$test successfully finished"
             ((success++))
