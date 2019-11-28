a943c820c8d6f447129312c2b11862ed9b5829e8
daniel.boehrs <daniel.boehrs@open-xchange.com>
2019-11-28 15:24:48 +0100

    OT-509 Update build script, update gradle, update libs

--- tools/build.triggerBuild.sh
+++ tools/build.triggerBuild.sh
@@ -1,13 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# System flags
 set -e
-set -o pipefail
 
+# User / CI input
+platforms=$1
+mode=$2
+flavor=$3
+buildName=$4
+buildNumber=$5
+
+# Constants
 PLUGIN_FOLDER="flutter-deltachat-core";
-BUILD_NAME="oxdev-r999.0"
+CORE_BUILD_SCRIPT="./build-dcc.sh"
 
-(
-    cd ..
+# Functions
+function isAndroid {
+    if [[ ${platforms} == "android" || ${platforms} == "all" ]]; then
+        true
+    else
+        false
+    fi
+}
+
+function isIos {
+    if [[ ${platforms} == "ios" || ${platforms} == "all" ]]; then
+        true
+    else
+        false
+    fi
+}
+
+function isRelease {
+    if [[ ${mode} == "release" ]]; then
+        true
+    else
+        false
+    fi
+}
+#flutter build apk --build-name=${BUILD_NAME} --build-number=${CI_PIPELINE_ID} --flavor development --debug
+function androidDebugBuild {
+    echo "Android debug build started"
+    flutter build apk --build-name=${buildName} --build-number=${buildNumber} --flavor ${flavor} --debug
+}
+
+function androidReleaseBuild {
+    echo "Android release build started"
+    flutter build apk --build-name=${buildName} --build-number=${buildNumber} --flavor ${flavor} --split-per-abi
+}
+
+function iosDebugBuild {
+    echo "iOS debug build started"
+    echo "TODO: iOS debug build is missing"
+}
+
+function iosReleaseBuild {
+    echo "iOS release build started"
+    echo "TODO: iOS release build is missing"
+}
+
+function getCore {
     if [[ -d "$PLUGIN_FOLDER" ]]; then
         cd ${PLUGIN_FOLDER}
         git pull
@@ -15,5 +67,41 @@ BUILD_NAME="oxdev-r999.0"
     else
         git clone --recurse-submodules https://github.com/open-xchange/flutter-deltachat-core.git
     fi
+}
+
+function buildCore {
+    if [[ -f "$CORE_BUILD_SCRIPT" ]]; then
+        if isAndroid; then
+            ${CORE_BUILD_SCRIPT} android | sed 's/^/    /'
+        elif isIos; then
+            ${CORE_BUILD_SCRIPT} ios | sed 's/^/    /'
+        fi
+     else
+        exit 2
+     fi
+}
+
+# Execution
+echo "-- Setup --"
+cd .. || error 1
+echo "Building / updating core"
+(
+    cd .. || error 3
+    getCore
+    buildCore
 )
-flutter build apk --build-name=${BUILD_NAME} --build-number=${CI_PIPELINE_ID} --flavor development --debug
+echo "-- Building --"
+if isAndroid; then
+    if isRelease; then
+        androidReleaseBuild
+    else
+        androidDebugBuild
+    fi
+elif isIos; then
+    if isRelease; then
+        iosReleaseBuild
+    else
+        iosDebugBuild
+    fi
+fi
+echo "-- Finishing --"
