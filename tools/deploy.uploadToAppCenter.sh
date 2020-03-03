fb1c5c086d2fa09d0024b78bd80aa773b675a64c
daniel.boehrs <daniel.boehrs@open-xchange.com>
2020-03-03 15:08:40 +0100

    OT-491 Fix deploy command

--- tools/deploy.uploadToAppCenter.sh
+++ tools/deploy.uploadToAppCenter.sh
@@ -3,9 +3,8 @@
 set -e
 set -o pipefail
 
-RELEASE_NOTES=`cat tools/deploy.releaseNotesHockeyDev.md`
 DISTRIBUTION_GROUP="Collaborators"
 APK="build/app/outputs/apk/development/debug/app-development-debug.apk"
-APPCENTER_APP="OX COI Messenger Dev"
+APP_CENTER_APP="OX COI Messenger Dev"
 
-appcenter "distribute release --app \"${APPCENTER_APP}\" --file ${APK} --group \"${DISTRIBUTION_GROUP}\" --output json --release-notes \"${RELEASE_NOTES}\""
+appcenter distribute release --app "${APP_CENTER_APP}" --file ${APK} --group ${DISTRIBUTION_GROUP} --output json
