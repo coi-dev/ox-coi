fc2663b941d16e857f0ca9f9444deb06eb55a830
daniel.boehrs <daniel.boehrs@open-xchange.com>
2020-03-03 14:53:13 +0100

    OT-491 Fix errors on space

--- tools/deploy.uploadToAppCenter.sh
+++ tools/deploy.uploadToAppCenter.sh
@@ -8,4 +8,4 @@ DISTRIBUTION_GROUP="Collaborators"
 APK="build/app/outputs/apk/development/debug/app-development-debug.apk"
 APPCENTER_APP="OX COI Messenger Dev"
 
-appcenter "distribute release --app ${APPCENTER_APP} --file ${APK} --group ${DISTRIBUTION_GROUP} --output json --release-notes ${RELEASE_NOTES}"
+appcenter "distribute release --app \"${APPCENTER_APP}\" --file ${APK} --group \"${DISTRIBUTION_GROUP}\" --output json --release-notes \"${RELEASE_NOTES}\""
