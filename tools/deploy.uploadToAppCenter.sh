a39e37dd72b41b58e14251b99913d5295cfdcb16
daniel.boehrs <daniel.boehrs@open-xchange.com>
2020-03-03 15:32:06 +0100

    OT-491 Adjust app name

--- tools/deploy.uploadToAppCenter.sh
+++ tools/deploy.uploadToAppCenter.sh
@@ -5,6 +5,6 @@ set -o pipefail
 
 DISTRIBUTION_GROUP="Collaborators"
 APK="build/app/outputs/apk/development/debug/app-development-debug.apk"
-APP_CENTER_APP="Open-Xchange/OX COI Messenger Dev"
+APP_CENTER_APP="open-xchange/OX-COI-Messenger-Dev"
 
 appcenter distribute release --app "${APP_CENTER_APP}" --file ${APK} --group ${DISTRIBUTION_GROUP} --output json
