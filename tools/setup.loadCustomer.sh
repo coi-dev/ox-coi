623dd035d60c0fd09b1a27f0355717a28b7e1c6e
Daniel <daniel.boehrs@open-xchange.com>
2020-05-15 10:07:14 +0200

    Api key exposed OT-721 #443

---
+++ tools/setup.loadCustomer.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# System flags
+set -e
+
+# User / CI input
+repository=$1
+
+# Constants
+SCRIPT_BASEDIR=$(dirname "$0")
+TEMP_CUSTOMER_FOLDER="customerSource"
+
+ANDROID_APP_FOLDER="android/app"
+
+IOS_APP_FOLDER="ios/OX Coi/"
+IOS_FIREBASE_FOLDER="${IOS_APP_FOLDER}/Firebase"
+
+# Setup
+if [[ "$#" != 1 ]]; then
+    echo "Usage of $0:"
+    echo
+    echo "1. parameter:     A git repository providing required customer files"
+    echo
+    echo "Example:  ./setup.loadCustomer https://gitlab.open-xchange.com/mobile/coi-customer-ox.git"
+    exit 0
+fi
+
+# Functions
+function error {
+    echo >&2 $1
+    exit $2
+}
+
+# Execution
+echo "-- Setup --"
+cd ${SCRIPT_BASEDIR} || error "Can't navigate into app directory" 1
+echo "-- Clean up --"
+rm -rf "${TEMP_CUSTOMER_FOLDER}"
+echo "-- Getting customer repository --"
+git clone $1 ${TEMP_CUSTOMER_FOLDER}
+echo "---------------------------"
+cat ${TEMP_CUSTOMER_FOLDER}/README.md
+echo "---------------------------"
+echo "-- Copy Firebase config --"
+cp "${TEMP_CUSTOMER_FOLDER}/${ANDROID_APP_FOLDER}/"* "../${ANDROID_APP_FOLDER}/"
+cp "${TEMP_CUSTOMER_FOLDER}/${IOS_FIREBASE_FOLDER}/"* "../${IOS_FIREBASE_FOLDER}/"
+echo "-- Finishing --"
